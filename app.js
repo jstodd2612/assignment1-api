567a1a00ab32217fd234fd526a6aa728a2d133e4
jstodd <jstodd2612@gmail.com>
2016-02-11 08:45:20 -0700

    Forgot to save files before commiting haha

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
+var reps = require('./lib/reps');
 var express = require('express');
-var reps = require('../lib/reps');
-
 var app = express();
 
-reps.allByZip('84043', function(err, results) {
- results.forEach(function(key) {
-   console.log(key.name);
- });
-});
+function makeRoute(route, method, param) {
+  app.get(route, function(req, res, next) {
+    method(req.params[param], function(err, people) {
+      if (err) { return next(err); }
+      res.json(people);
+    });
+  });
+}
 
-app.get('/', function(req, res) {
- res
-   .status(200)
-   .send('This Api is Working!');
-});
+makeRoute('/all/by-zip/:zip', reps.allByZip, 'zip');
+makeRoute('/reps/by-name/:name', reps.repsByName, 'name');
+makeRoute('/reps/by-state/:state', reps.repsByState, 'state');
+makeRoute('/sens/by-name/:name', reps.sensByName, 'name');
+makeRoute('/sens/by-state/:state', reps.sensByState, 'state');
 
-app.get('/all/by-zip/:zip', function(req,res,next){
-  resp.allByZip(req.params.zip);
-});
-
-app.get('/reps/by-name/:name', function(req,res,next){
-
-});
-
-app.get('/reps/by-state/:state', function(req,res,next){
-
-});
-
-app.get('/sens/by-name/:name', function(req,res,next){
-
-});
-
-app.get('/sens/by-state/:state', function(req,res,next){
-
-});
-
-
-
-app.listen(8000, function() {
- console.log('Listening on port 8000.');
-});
+module.exports = app;
