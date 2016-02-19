d2a241404ff0cccd3eee0cdde608f6c389f362fe
jstodd <jstodd2612@gmail.com>
2016-02-19 13:08:05 -0700

    Part 3 of Assignment

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 var reps = require('./lib/reps');
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// app.set('view cache', false);
+
+
+app.get('/', function(req, res, next){
+  var method;
+
+  switch (req.query.type) {
+    case 'zip': method = reps.allByZip; break;
+    case 'name': method = reps.repsByName; break;
+    case 'state': method = reps.repsByState; break;
+    default: method = function(param, cb) { cb(null, []); }; break;
+  }
+
+  if (method) {
+    method(req.query.search, function(err, people){
+      if (err) { return next(err); }
+      res.render('index', {
+        reps: people,
+        query: req.query,
+      });
+    });
+  }
+  // res.render('index', {
+  //   foo: 'bar',
+  //   arr: [1,4,7,3,9]
+  // });
+});
+
 
 function makeRoute(route, method, param) {
   app.get(route, function(req, res, next) {
@@ -10,6 +42,10 @@ function makeRoute(route, method, param) {
     });
   });
 }
+
+app.listen(8000, function(){
+  console.log('Listening on port 8000');
+});
 
 makeRoute('/all/by-zip/:zip', reps.allByZip, 'zip');
 makeRoute('/reps/by-name/:name', reps.repsByName, 'name');
